3109fe8619e4f7e74748ec4b93ae413ab31a99f1
kieuxuanduong <duongkieu91@gmail.com>
2020-06-22 23:44:52 +0700

    add ChatApp register screen

---
+++ ChatApp/js/components.js
@@ -0,0 +1,40 @@
+const components = {}
+
+components.welcomeScreen = `
+    <div> Welcome to chat app </div>
+`
+components.registerScreen = `
+<div class="register-container">
+            <div class="register-form">
+                <div class="title">Mindx Chat</div>
+                <form id="form-register">
+                    <div class="name-wrapper">
+                        <div class="input-wrapper">
+                            <input type="text" name="firstName" placeholder="First Name..." id="">
+                            <div class="error" id="error-first-name"></div>
+                        </div>
+                        <div class="input-wrapper">
+                            <input type="text" name="lastName" placeholder="Last Name..." id="">
+                            <div class="error" id="error-last-name"></div>
+                        </div>
+                    </div>
+                    <div class="input-wrapper">
+                        <input type="email" name="email" placeholder="mail@example.com" id="">
+                        <div class="error" id="error-email"></div>
+                    </div>
+                    <div class="input-wrapper">
+                        <input type="password" name="password" placeholder="Password...">
+                        <div class="error" id="error-password"></div>
+                    </div>
+                    <div class="input-wrapper">
+                        <input type="password" name="confirmPassword" placeholder="Confirm Password...">
+                        <div class="error" id="error-confirm-password"></div>
+                    </div>
+                    <div class="submit-wrapper">
+                        <div>Already have an account? <span class="cursor-pointer" id="redirect-to-login">Login</span></div>
+                        <button class="btn" type="submit">Register</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+`
---
+++ ChatApp/js/view.js
@@ -0,0 +1,23 @@
+const view = {}
+
+view.setActiveScreen = (screenName) => {
+    document.getElementById('app').innerHTML=components.welcomeScreen
+    switch(screenName)
+    {
+        case 'registerScreen':
+            document.getElementById('app').innerHTML = components.registerScreen
+            const registerForm = document.getElementById('form-register')
+            registerForm.addEventListener('submit',(e)=>{
+                e.preventDefault()
+                const registerInfo = {
+                    firstName: registerForm.firstName.value,
+                    lastName: registerForm.lastName.value,
+                    email: registerForm.email.value,
+                    password: registerForm.password.value,
+                    confirmPassword: registerForm.confirmPassword.value,   
+                }
+                controller.register(registerInfo)
+            })
+            break
+    }
+}
